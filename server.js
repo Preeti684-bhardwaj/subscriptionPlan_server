683d1814b00d73ec59db440a881b47b684f31b66
Preeti Bhardwaj <bhardwajpreeti@gmail.com>
2024-05-13 12:19:19 +0530

    prerender added

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 const express = require("express");
 const app = express();
-const rendertron = require('rendertron-middleware');
-// const PORT = process.env.PORT || 8080;
-const DIST_FOLDER = process.cwd() + '/public';
 const cors = require('cors');
 const Stripe = require("stripe");
 const dotenv = require('dotenv').config();
@@ -13,27 +10,10 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(cors());
 
-// Add googlebot to the list of bots we will use Rendertron for
-const BOTS = rendertron.botUserAgents.concat('googlebot');
-const BOT_UA_PATTERN = new RegExp(BOTS.join('|'), 'i');
+// // Static Assets
+// app.get('*.*', express.static('public'));
 
-app.set('view engine', 'html');
-
-// Add Rendertron middleware to send bot requests to Rendertron
-app.use(rendertron.makeMiddleware({
-  proxyUrl: 'https://render-tron.appspot.com/render', // this is a DEMO URL! Do not use this in production!
-  userAgentPattern: BOT_UA_PATTERN
-}));
-
-// Static Assets
-app.get('*.*', express.static('public'));
-
-// Point all routes to index...
-app.get('*', (req, res) => {
-  res.set('Vary', 'User-Agent');
-  res.sendFile(DIST_FOLDER + '/index.html');
-});
-
+app.use(require('prerender-node').set('prerenderToken', `${process.env.PRERENDER_TOKEN}`));
 
 app.post("/create-checkout-session", async (req, res) => {
     console.log(req.body);
