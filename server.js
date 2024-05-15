c5928ca957f3aaeb4d09915072042dc4d3ffde06
Preeti Bhardwaj <bhardwajpreeti@gmail.com>
2024-05-15 14:11:49 +0530

    build added

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.post('/check-ip',async(req,res)=>{
 });
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname ,'public','index.html'));
+    res.sendFile(path.join(__dirname ,'build','index.html'));
 })
 
 app.post("/create-checkout-session", async (req, res) => {
